2092e19cc1a9fd506948c0dbd271c3753a7699ca
Alex <alexsandrolima991@hotmail.com>
2022-08-27 13:01:48 -0300

    requisito 1

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
 // Coloque aqui suas actions
+export const USER_EMAIL = 'USER_EMAIL';
+
+export function emailValue(payload) {
+  return {
+    type: USER_EMAIL,
+    payloaduser: payload,
+  };
+}
+
+export default emailValue;
